518e090607f48b057cfd713924bd2266466f6e2f
anhskrttt <duonanh195@gmail.com>
2022-08-21 21:51:49 -0700

    initial api

--- go-server/main.go
+++ go-server/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Hello, World")
+
+	// Init Router.
+	r := mux.NewRouter()
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("/", HomePageHandler).Methods("GET")
+
+	// Start server.
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
+
+func HomePageHandler(w http.ResponseWriter, r *http.Request) {
+	welcome := "Welcome to my homepage"
+	json.NewEncoder(w).Encode(&welcome)
+}
+
+// type ToDo struct {
+// 	ID int `json: "id"`
+// 	Name string `json: "name"`
+// 	Description string `json: "description"`
+// 	Status bool `json: "status"`
+// }
